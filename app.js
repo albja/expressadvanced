6dde64af3a278d60c9891ee6b646e09f3a996f2c
Albja <alban.jaunet@sncf.fr>
2023-02-06 11:17:01 +0100

    correction in app.js

--- app.js
+++ app.js
@@ -14,36 +14,31 @@ const welcome = (req, res) => {
     res.send("Welcome to my favourite movie list");
 };
 
-app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
 // the public routes
 
 
+app.get("/", welcome);
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
 app.post(
     "/api/login",
     userHandlers.getUserByEmailWithPasswordAndPassToNext,
     verifyPassword
 );
+app.post("/api/users", hashPassword, userHandlers.postUser);
 
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
 // then the routes to protect
 
 app.use(verifyToken);
 app.post("/api/movies", verifyToken, movieHandlers.postMovie);
-
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
-
-
-
 app.delete("/api/users/:id", userHandlers.deleteUser);
-
-app.post("/api/users", hashPassword, userHandlers.postUser);
 app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
 
 app.listen(port, (err) => {
